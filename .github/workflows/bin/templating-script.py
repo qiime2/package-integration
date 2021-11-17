76dc71361402e38cf2da5ce978816bc268b88465
Liz Gehret <elizabeth.gehret@gmail.com>
2021-11-17 10:26:48 -0700

    setting template script as executable in git

--- .github/workflows/bin/templating-script.py
+++ .github/workflows/bin/templating-script.py
@@ -4,15 +4,6 @@ import sys
 from jinja2 import Template
 
 
-def validate_epoch_input(cli_args):
-    try:
-        epoch = cli_args[1]
-    except Exception:
-        raise ValueError('Missing required parameter. Epoch must be provided.')
-
-    return epoch
-
-
 def write_recipe(jinja):
     epoch = validate_epoch_input(sys.argv)
     with open(jinja) as fh:
@@ -25,5 +16,13 @@ def write_recipe(jinja):
 
 
 if __name__ == '__main__':
+
+    def validate_epoch_input(cli_args):
+        try:
+            epoch = cli_args[1]
+        except Exception:
+            raise ValueError('Missing required parameter. Epoch must be provided.')
+
+        return epoch
 
     write_recipe('cron-template.j2')
