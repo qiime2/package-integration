dd6e17fc79a5f0c0a93a2fbfab96846d06def54e
Liz Gehret <elizabeth.gehret@gmail.com>
2021-11-18 11:19:45 -0700

    IMP: some additional refactors, elision & cron jinja template additions from @keeganevans commit #061dd63

--- .github/workflows/bin/generate_cron_workflows.py
+++ .github/workflows/bin/generate_cron_workflows.py
@@ -4,32 +4,20 @@ import sys
 from jinja2 import Environment, FileSystemLoader
 
 
-def validate_inputs(cli_args):
+if __name__ == '__main__':
+
     try:
-        epoch = cli_args[1]
+        epoch = sys.argv[1]
     except Exception:
         raise ValueError('Missing required parameter. '
                          'Epoch must be provided.')
 
-    return epoch
-
-
-if __name__ == '__main__':
-
-    def write_recipe(jinja):
-        epoch = validate_inputs(sys.argv)
-        for os in 'macos', 'ubuntu':
-            filename = 'cron-' + epoch + '-core-' + os + '-latest.yaml'
-            env = Environment(loader=FileSystemLoader('templates'),
-                              variable_start_string='{{{',
-                              variable_end_string='}}}')
-            template = env.get_template(jinja)
-            with open(filename, 'w') as outfile:
-                if os == 'macos':
-                    opsys = 'osx'
-                else:
-                    opsys = 'linux'
-                outfile.write(template.render(input_epoch=epoch,
-                                              input_os=os, op_sys=opsys))
-
-    write_recipe('cron-workflow-template.j2')
+    for os, opsys in [('macos', 'osx'), ('ubuntu', 'linux')]:
+        filename = 'cron-' + epoch + '-core-' + os + '-latest.yaml'
+        env = Environment(loader=FileSystemLoader('templates'),
+                          variable_start_string='{{{',
+                          variable_end_string='}}}')
+        template = env.get_template('cron-workflow-template.j2')
+        with open(filename, 'w') as outfile:
+            outfile.write(template.render(input_epoch=epoch,
+                                          input_os=os, op_sys=opsys))
